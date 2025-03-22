b85ef16370fa3f163c44ed31c63e367fe522073a
Jose Javier Vegas <65979544+vegasjj@users.noreply.github.com>
2025-03-21 23:37:48 -0400

    updated json structure from the azure function api

--- docs/static/visit-counter.js
+++ docs/static/visit-counter.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch('https://webresume-to-counter.azurewebsites.net/api/webresume_to_counter?')
         .then(response => response.json())
         .then(data => {
-            document.getElementById('visit-counter').innerText = data.count;
+            document.getElementById('visit-counter').innerText = data.visits_counter;
         })
         .catch(error => console.error('Error fetching visit counter:', error));
 });
