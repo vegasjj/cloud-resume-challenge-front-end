7c79a131a0f16aea5e8986886de06e6455f98a7a
Jose Javier Vegas <vegasjj@gmail.com>
2025-08-15 23:20:54 -0400

    Add gitignore file for local environment

--- main.tf
+++ main.tf
@@ -32,7 +32,6 @@ resource "azurerm_storage_account" "staticweb" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-#   allow_blob_public_access = false
 
   static_website {
     index_document = "index.html"
